6350f26e5f4e682f639d754bc988be4f084aecca
GenTelGuy <=>
2020-04-18 00:48:44 -0700

    Implement Basic Enemy Movement

--- Enemies/BasicEnemy.gd
+++ Enemies/BasicEnemy.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 const BULLET_KNOCKBACK = 64
 
 var bullet_scene = load("res://Bullets/Bullet.tscn")
+var player_scene = load("res://Player/Player.tscn")
+
+
 
 onready var velocity : Vector2 = Vector2()
 onready var knockback_velocity : Vector2 = Vector2()
@@ -11,12 +14,27 @@ onready var health = 2
 
 onready var shoot_timer = $ShootTimer
 onready var hitbox = $Hitbox
+onready var movement_interval_timer = $MovementIntervalTimer
+onready var movement_duration_timer = $MovementDurationTimer
+
+onready var PREFERRED_DISTANCE_FROM_CLOSEST_ADVERSARY = 200
+onready var SPEED = 500
+onready var MOVEMENT_DURATION = 0.2
 
 func _ready():
     shoot_timer.connect("timeout", self, "_shoot_cooldown_finished")
     shoot_timer.one_shot = false
     shoot_timer.wait_time = 2
     shoot_timer.start()
+    
+    movement_interval_timer.connect("timeout", self,  "_begin_movement")
+    movement_interval_timer.one_shot = false
+    movement_interval_timer.wait_time = 5
+    movement_interval_timer.start()
+    
+    movement_duration_timer.connect("timeout", self, "_end_movement")
+    movement_duration_timer.one_shot = true
+    
     
     hitbox.connect("body_entered", self, "_on_body_entered")
 
@@ -32,11 +50,31 @@ func shoot_bullet() -> void:
 func knockback(x : float, y : float) -> void:
     knockback_velocity.x = x
     knockback_velocity.y = y
+    
+onready var random_offset_factor = 0.2;
+    
+func get_new_movement_direction() -> Vector2:
+    var distance = global_position.distance_to(get_coords_of_closest_adversary())
+    var go_towards_adversary
+    if distance <= PREFERRED_DISTANCE_FROM_CLOSEST_ADVERSARY:
+        go_towards_adversary = -1
+    else:
+        go_towards_adversary = 1
+    var direction = go_towards_adversary * global_position.direction_to(get_coords_of_closest_adversary())
+    var random_offset = Vector2(randf(), randf()).normalized();
+    return (direction + random_offset * random_offset_factor).normalized();
+
+    
+func get_coords_of_closest_adversary() -> Vector2:
+    # Todo also have the monster included in this consideration
+    var player = get_tree().get_nodes_in_group("Players")[0]
+    return player.global_position 
 
 func damage() -> void:
     health -= 1
     if health <= 0:
         queue_free()
+        
 
 func _physics_process(delta):
     # Knockback velocity is reduced
@@ -47,6 +85,14 @@ func _physics_process(delta):
 
 func _shoot_cooldown_finished():
     shoot_bullet()
+    
+func _begin_movement():
+    velocity = get_new_movement_direction() * SPEED
+    movement_duration_timer.wait_time = MOVEMENT_DURATION
+    movement_duration_timer.start()
+    
+func _end_movement():
+    velocity = Vector2.ZERO
 
 func _on_body_entered(body):
     if body.is_in_group("Players"):
