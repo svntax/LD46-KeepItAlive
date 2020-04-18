7f35081640dbfd0be4bb2741dfd50d04d76b8b83
svntax <endless.answers@gmail.com>
2020-04-18 02:39:33 -0400

    Added melee attack that reflects bullets.

--- Bullets/Bullet.gd
+++ Bullets/Bullet.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 onready var velocity : Vector2 = Vector2()
+onready var reflected_velocity : Vector2 = Vector2() 
 
 onready var animation_player = $AnimationPlayer
-# TODO: play the reflect animation when the bullet is reflected
 
 func _ready():
     #animation_player.play("rest")
@@ -11,11 +11,18 @@ func _ready():
     velocity.x = 1
     velocity.y = 1
 
+func reflect_back(vec : Vector2) -> void:
+    reflected_velocity = vec * 2
+    set_velocity(vec.x, vec.y)
+    animation_player.play("reflect")
+
 func set_velocity(x : float, y : float) -> void:
     velocity.x = x
     velocity.y = y
 
 func _physics_process(delta):
-    var collision = move_and_collide(velocity)
+    var collision = move_and_collide(velocity + reflected_velocity)
     if collision:
         velocity = velocity.bounce(collision.normal)
+    if reflected_velocity.length() > 0:
+        reflected_velocity = reflected_velocity.linear_interpolate(Vector2.ZERO, 0.15)
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
 const WALK_SPEED = 64
+const REFLECT_SPEED = 4
 
 enum State {NORMAL, STUNNED}
 onready var current_state = State.NORMAL
 
 onready var velocity : Vector2 = Vector2()
 
+onready var sword_angle = 0
+
 onready var animation_player = $AnimationPlayer
+onready var sword_player = $SwordPlayer
 onready var stunned_timer = $StunnedTimer
+onready var sword_pivot = $SwordPivot
 
 func _ready():
     pass
@@ -27,7 +32,20 @@ func stun() -> void:
     if current_state != State.STUNNED:
         change_state(State.STUNNED)
 
+func update_sword():
+    if sword_player.current_animation != "slash":
+        sword_angle = sword_pivot.global_position.angle_to_point(get_global_mouse_position())
+        sword_pivot.set_global_rotation(sword_angle)
+
+func reset_hilt():
+    sword_pivot.get_node("HiltPivot").position = Vector2(0, 0)
+
 func _physics_process(delta):
+    update_sword()
+    if Input.is_action_just_pressed("SLASH"):
+        if !sword_player.is_playing():
+            sword_player.play("slash")
+    
     velocity.x = 0
     velocity.y = 0
     if Input.is_action_pressed("MOVE_LEFT"):
@@ -52,3 +70,12 @@ func _on_Hitbox_body_entered(body):
     if body.is_in_group("Bullets"):
         stun()
         body.queue_free()
+
+func _on_SwordPlayer_animation_finished(anim_name):
+    if anim_name == "slash":
+        reset_hilt()
+
+func _on_Sword_body_entered(body):
+    if body.is_in_group("Bullets"):
+        var push = Vector2(-1, 0).rotated(sword_angle) * REFLECT_SPEED
+        body.reflect_back(push)
