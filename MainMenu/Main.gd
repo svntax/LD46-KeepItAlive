7a025690938b8d154b8705ca871ed17c8edd5f67
svntax <endless.answers@gmail.com>
2020-04-17 22:34:57 -0400

    Adjusted resolution and linked main menu to gameplay scene.

--- MainMenu/Main.gd
+++ MainMenu/Main.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
 func _ready():
-	pass # Replace with function body.
+    pass
+
+func _process(delta):
+    if Input.is_action_just_pressed("ui_accept"):
+        get_tree().change_scene("res://GameplayScene/Gameplay.tscn")
